4237a0a813af42484cd3630c7b1263ff5c6ae2d8
jingyu <LiTrevize@github.com>
2021-05-06 19:45:57 +0800

    feat: query by metrics name in time range or latest

--- influxdb_client.go
+++ influxdb_client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
@@ -13,6 +14,8 @@ type InfluxdbClient struct {
 	Client   influxdb2.Client
 	WriteAPI api.WriteAPIBlocking
 	QueryAPI api.QueryAPI
+	Org      string
+	Bucket   string
 }
 
 func NewInfluxdbClient() *InfluxdbClient {
@@ -20,10 +23,11 @@ func NewInfluxdbClient() *InfluxdbClient {
 	url := "http://127.0.0.1:8086"
 	token := "KhYF9SoMOHcb9m8DJnrwRH5LVwoRLy-YVGmZKSYegp6LC5CEiKoksGFF2KK4bZFx1gHiS4lCZKDP1V9kN2oUrw=="
 	ic.Client = influxdb2.NewClient(url, token)
-	org := "SJTU"
-	bucket := "metrics"
-	ic.WriteAPI = ic.Client.WriteAPIBlocking(org, bucket)
-	ic.QueryAPI = ic.Client.QueryAPI(org)
+	ic.Org = "SJTU"
+	ic.Bucket = "metrics"
+
+	ic.WriteAPI = ic.Client.WriteAPIBlocking(ic.Org, ic.Bucket)
+	ic.QueryAPI = ic.Client.QueryAPI(ic.Org)
 
 	return ic
 }
@@ -54,14 +58,13 @@ func (ic *InfluxdbClient) WriteMetricsFromLog(log string) {
 
 func (ic *InfluxdbClient) queryMetricsExample() {
 	result, err := ic.QueryAPI.Query(context.Background(), `from(bucket:"metrics")
-    |> range(start: -1h)`)
-	fmt.Println(result)
+    |> range(start: -1d)`)
 	if err == nil {
 		for result.Next() {
 			if result.TableChanged() {
-				fmt.Printf("table: %s\n", result.TableMetadata().String())
+				fmt.Printf("table: %s\n", result.Record().Measurement())
 			}
-			fmt.Printf("value: %v\n", result.Record().Value())
+			fmt.Printf("%v: %v\n", result.Record().Field(), result.Record().Value())
 		}
 		if result.Err() != nil {
 			fmt.Printf("query parsing error: %s\n", result.Err().Error())
@@ -69,5 +72,78 @@ func (ic *InfluxdbClient) queryMetricsExample() {
 	} else {
 		panic(err)
 	}
+}
 
+func (ic *InfluxdbClient) queryMetricsLatest(name string) *MetricsLog {
+	ml := &MetricsLog{Name: name,
+		Tag:   make(map[string]string, 3),
+		Field: make(map[string]interface{}, 5)}
+	setTag := false
+	result, err := ic.QueryAPI.Query(context.Background(), fmt.Sprintf(`from(bucket:"%s")
+    |> range(start: -1d)
+	|> filter(fn: (r) => r._measurement == "%s")
+	|> last()`, ic.Bucket, name))
+	if err == nil {
+		for result.Next() {
+			if !setTag {
+				for k, v := range result.Record().Values() {
+					if strings.HasPrefix(k, "_") || k == "result" || k == "table" {
+						continue
+					}
+					ml.Tag[k] = v.(string)
+				}
+				setTag = true
+			}
+			ml.Field[result.Record().Field()] = result.Record().Value()
+		}
+		if result.Err() != nil {
+			fmt.Printf("query parsing error: %s\n", result.Err().Error())
+		}
+	} else {
+		fmt.Printf("query error: %s\n", err)
+	}
+	return ml
+}
+
+func (ic *InfluxdbClient) queryMetricsRange(name string, start string) *MetricsSeries {
+	ms := NewMetricsSeries(name)
+	setTag := false
+	result, err := ic.QueryAPI.Query(context.Background(), fmt.Sprintf(`from(bucket:"%s")
+    |> range(start: %s)
+	|> filter(fn: (r) => r._measurement == "%s")`, ic.Bucket, start, name))
+	if err == nil {
+		for result.Next() {
+			if !setTag {
+				for k, v := range result.Record().Values() {
+					if strings.HasPrefix(k, "_") || k == "result" || k == "table" {
+						continue
+					}
+					ms.Tag[k] = v
+				}
+				setTag = true
+			}
+			ms.Values = append(ms.Values, result.Record().Value())
+			ms.Time = append(ms.Time, result.Record().Time())
+		}
+		if result.Err() != nil {
+			fmt.Printf("query parsing error: %s\n", result.Err().Error())
+		}
+	} else {
+		fmt.Printf("query error: %s\n", err)
+	}
+	return ms
+}
+
+type MetricsSeries struct {
+	Name   string                 `json:"name"`
+	Tag    map[string]interface{} `json:"tag"`
+	Values []interface{}          `json:"values"`
+	Time   []time.Time            `json:"time"`
+}
+
+func NewMetricsSeries(name string) *MetricsSeries {
+	return &MetricsSeries{name,
+		make(map[string]interface{}, 3),
+		make([]interface{}, 0, 10),
+		make([]time.Time, 0, 10)}
 }
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	ic := NewInfluxdbClient()
-	ic.writeMetricsExample()
-	ic.queryMetricsExample()
-	ic.WriteMetricsFromLog(`{"name":"cpu_usage_nano_cores","tag":{"node":"ubuntu16"},"val":195703672,"time":"2021-05-03T00:16:09Z"}`)
+	// ic.writeMetricsExample()
+	// ic.WriteMetricsFromLog(`{"name":"cpu_usage_nano_cores","tag":{"node":"ubuntu16"},"val":195703672,"time":"2021-05-06T14:07:13.234253206+08:00"}`)
+	// ic.WriteMetricsFromLog(`{"name":"gpu_device_info","tag":{"GPU":"0","UUID":"GPU-25be9bfa-ba12-0ab4-5fee-b1c4763a8eef","model":"GeForce GTX 1080 Ti","node":"ubuntu16"},"val":null,"time":"2021-05-06T14:07:13.234253206+08:00","field":{"bandwidth":"15760 MB/s","clock_cores":"1911 MHz","clock_memory":"5505 MHz","memory":"11178 MiB","power":"250 W"}}`)
+	ms := ic.queryMetricsRange("cpu_usage_nano_cores", "-1d")
+	fmt.Printf("%+v\n", ms)
+	ml := ic.queryMetricsLatest("gpu_device_info")
+	fmt.Printf("%+v\n", ml)
 }
