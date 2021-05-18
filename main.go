191c622c664a068bbcd3231746d95bbd398ace12
jingyu <LiTrevize@github.com>
2021-05-18 16:05:55 +0800

    feat: add api to show all metrics name

--- main.go
+++ main.go
@@ -7,10 +7,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const AllMetrics string = `node_cpu_usage_nano_cores
+node_memory_usage_bytes
+pod_cpu_usage_nano_cores
+pod_memory_usage_bytes
+gpu_power
+gpu_temperature
+gpu_util
+gpu_memory_util
+gpu_device_info
+`
+
 func main() {
 	ma := NewMetricsAggregator()
 	fmt.Println(ma.LastAggregateTimeForNode("ubuntu16"))
-	mlogs := ma.IC.queryMetricsRange("pod_memory_usage_bytes", "-1d")
+	mlogs := ma.IC.queryMetricsRange("pod_memory_usage_bytes", "-5d")
 	for _, mlog := range mlogs {
 		fmt.Printf("%+v\n", mlog)
 	}
@@ -20,6 +31,9 @@ func main() {
 	r := gin.Default()
 	// 2.绑定路由规则，执行的函数
 	// gin.Context，封装了request和response
+	r.GET("/metrics", func(c *gin.Context) {
+		c.String(http.StatusOK, AllMetrics)
+	})
 	r.GET("/metrics/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		start := c.DefaultQuery("start", "")
@@ -28,6 +42,7 @@ func main() {
 			c.JSON(http.StatusOK, map[string][]*MetricsLog{"data": mlogs})
 		} else {
 			mlogs := ma.IC.queryMetricsRange(name, start)
+			fmt.Printf("%+v\n", mlogs)
 			c.JSON(http.StatusOK, map[string][]*MetricsSeries{"data": mlogs})
 		}
 	})
